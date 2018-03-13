b20cc75b904369def5246ed3249b45f2b8df905e
Rifai Kridolaksono <rivayx@gmail.com>
2018-03-13 20:33:14 +0700

    First Commit

---
+++ Assets/Scripts/Core/BaseApps.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+namespace BaseApps
+{
+    public struct CONST_VAR
+    {
+        private const string STATUS_AKUN = "starter/";
+        private const string API_ONGKIR = "https://api.rajaongkir.com/" + STATUS_AKUN;
+
+        public const string API_KEY = "21275b6a203cca49016095c7810bf462";
+        public const string GET_CITY = API_ONGKIR + "city?key=" + API_KEY;
+        public const string GET_COST = API_ONGKIR + "cost";
+    }
+}
---
+++ Assets/Scripts/Core/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using BaseApps;
+
+public class Singleton : MonoBehaviour
+{
+
+    public static Singleton Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+            if (Instance == null)
+            {
+                Instantiate(Instance);
+                Debug.Log("error when trying to create singleton");
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
